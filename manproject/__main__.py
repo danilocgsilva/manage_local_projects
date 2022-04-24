1bcdd8b07e403d5e806c63e0c88fa43e30d52628
Danilo Silva <danilocgsilva@gmail.com>
2022-04-24 10:58:20 -0300

    Validation and gentle message
    Some commands needs the project name. In this scenario,
    now the command halts te execution with a gentle message
    instead of throwing an exception that may be meaningless
    to the user.

--- manproject/__main__.py
+++ manproject/__main__.py
@@ -1,4 +1,5 @@
 import sys
+from wsgiref import validate
 from manproject.commands_bag import commands_bag
 
 def main():
@@ -18,9 +19,17 @@ def execute_command(command: str):
             argument = sys.argv[2]
         else:
             argument = None
-        commands_bag[command]["command"](argument)
+
+        if validate_command_requirements(commands_bag[command]):
+            commands_bag[command]["command"](argument)
+        else:
+            print("The commands needs an additional argument to be the project name. Eg.: mpro {} <project_name>.".format(command))
 
     else:
         print("A don't know which is this command.")
 
-    
+def validate_command_requirements(command) -> bool:
+    if command["requires_argument"]:
+        if len(sys.argv) < 3:
+            return False
+    return True
