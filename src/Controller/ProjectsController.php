e4a15d2a057c6ff5a72e20ebe95b34630ca79589
Danilo Silva <danilocgsilva@gmail.com>
2023-07-28 11:11:13 -0300

    First entity and migration

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -5,12 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Project;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ProjectsController extends AbstractController
 {
     #[Route('/projects', name: 'app_projects')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $projectRepository = $entityManager->getRepository(Project::class);
+        
         return $this->render('projects/index.html.twig', [
             'environmentMode' => $this->getParameter('kernel.environment')
         ]);
